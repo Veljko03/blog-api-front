22192ea133e1a1aa6b63be83ed14ab8fd516e532
Veljko03 <veljkodovragovic@gmail.com>
2025-01-20 22:20:37 +0100

    getching blogs from backend

--- clientFrontend/src/App.jsx
+++ clientFrontend/src/App.jsx
@@ -1,15 +1,32 @@
-import { Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-const Profile = () => {
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/posts", { mode: "cors" })
+      .then((response) => response.json())
+      .then((res) => setBlogs(res))
+      .catch((error) => console.error(error));
+  }, []);
+  console.log(blogs);
+
   return (
     <div>
-      <h1>Hello from profile page!</h1>
-      <p>So, how are you?</p>
-      <hr />
-      <h2>The profile visited is here:</h2>
-      <Outlet />
+      <h1>Hello from the main page of the app!</h1>
+      <p>Here are all the blogs</p>
+      {blogs.map((blog) => (
+        <div key={blog.id}>Title: {blog.title}</div>
+      ))}
+      <nav>
+        <ul>
+          <li>
+            <Link to="profile">Profile page</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
 
-export default Profile;
+export default App;
