0761225ab950423579360e27cb51970953a1bc55
Veljko03 <veljkodovragovic@gmail.com>
2025-01-23 14:47:54 +0100

    implemented editing blogs and saving those changes

--- adminFrontend/src/SingleBlog.jsx
+++ adminFrontend/src/SingleBlog.jsx
@@ -4,12 +4,14 @@ import { useState, useEffect } from "react";
 const Blog = () => {
   const { postId } = useParams();
   const [blog, setBlog] = useState(null);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
   const [comments, setComments] = useState([]);
-  const [numOfLikes, setNumOfLikes] = useState(0);
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
   const [token, setToken] = useState("");
-  const [text, setCommentTxt] = useState("");
+  //const [text, setCommentTxt] = useState("");
   const [triggerReload, setTriggerReload] = useState(false);
 
   useEffect(() => {
@@ -24,90 +26,21 @@ const Blog = () => {
   }, []);
 
   useEffect(() => {
+    if (blog) {
+      setTitle(blog.title);
+      setContent(blog.post_text);
+    }
     fetch(`http://localhost:3000/posts/${postId}`, { mode: "cors" })
       .then((response) => response.json())
       .then((res) => {
         setBlog(res);
         setComments(res.comments || []);
-        setNumOfLikes(res.num_of_likes);
+        setTriggerReload((prev) => !prev);
+
+        //setNumOfLikes(res.num_of_likes);
       })
       .catch((error) => console.error(error));
   }, [triggerReload]);
-
-  const handleLike = (event) => {
-    event.preventDefault();
-    if (token) {
-      const userID = user.id;
-      console.log(userID);
-
-      fetch(`http://localhost:3000/posts/like/${postId}`, {
-        method: "post",
-
-        headers: new Headers({
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        }),
-        body: JSON.stringify({ userID }),
-      })
-        .then((response) => {
-          if (!response.ok) {
-            alert("Invalid credentials");
-            throw new Error("Invalid credentials");
-          }
-          return response.json();
-        })
-        .then((data) => {
-          if (data) {
-            setNumOfLikes(data.num_of_likes);
-          } else {
-            throw new Error("Login failed: no token provided");
-          }
-        })
-        .catch((err) => console.log(err));
-    } else {
-      alert("log in first");
-      navigate("/log-in");
-    }
-  };
-
-  const hadleCommentAdd = (event) => {
-    event.preventDefault();
-
-    if (token) {
-      const userID = user.id;
-      const forBody = { text, userID };
-
-      fetch(`http://localhost:3000/posts/comment/${postId}`, {
-        method: "post",
-
-        headers: new Headers({
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        }),
-        body: JSON.stringify(forBody),
-      })
-        .then((response) => {
-          if (!response.ok) {
-            alert("Invalid credentials");
-            //navigate("/log-in");
-            throw new Error("Invalid credentials");
-          }
-          return response.json();
-        })
-        .then((data) => {
-          if (data) {
-            setCommentTxt("");
-            setTriggerReload((prev) => !prev);
-          } else {
-            throw new Error("Login failed: no token provided");
-          }
-        })
-        .catch((err) => console.log(err));
-    } else {
-      alert("log in first");
-      navigate("/log-in");
-    }
-  };
 
   const handleDeleteComment = (commentID) => {
     const userID = user.id;
@@ -136,29 +69,65 @@ const Blog = () => {
       .catch((err) => console.log(err));
   };
 
+  const handleEdit = (event) => {
+    event.preventDefault();
+
+    const forBody = { title, content };
+    console.log(forBody);
+
+    fetch(`http://localhost:3000/posts/${blog.id}`, {
+      method: "put",
+
+      headers: new Headers({
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      }),
+      body: JSON.stringify(forBody),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          //alert("Invalid credentials");
+          throw new Error("Invalid credentials", response);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data) {
+          alert("post updated");
+          navigate("/");
+        } else {
+          throw new Error("Login failed: no token provided");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
   if (blog) {
     return (
       <div>
         <h1>Here is specific blog!</h1>
-        <h2>{blog.title}</h2>
-        <p>{blog.post_text}</p>
-        <br />
-        <br />
-        <h4>{numOfLikes} likes</h4>
-        <button onClick={handleLike}>like/unlike</button>
-        <h3>Comments:</h3>
-        <br />
-        {/* <form onSubmit={hadleCommentAdd}>
+        <h1>You can edit it here</h1>
+        <form onSubmit={handleEdit}>
+          <h2>Title:</h2>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <h2>Content:</h2>
           <textarea
-            value={text}
-            onChange={({ target }) => setCommentTxt(target.value)}
-            placeholder="Your comment"
+            defaultValue={content}
+            onChange={(e) => setContent(e.target.value)}
           ></textarea>
-          <button type="submit"> Submit</button>
-        </form> */}
+          <br />
+          <button type="submit">Save changes</button>
+        </form>
+
+        <br />
+        <h3>Comments:</h3>
+
         {comments.map((comment) => (
           <div key={comment.id}>
-            <br />
             {comment.comment_text}
             {comment.user_id == user?.id && (
               <button onClick={() => handleDeleteComment(comment.id)}>
